fff97acf76fa877deca0c490901ca71ca4b8129b
陆翔宇 <2422350954@qq.com>
2023-03-29 15:22:45 +0800

    提交文件

---
+++ src/views/layout/footer/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer">
+    &nbsp;&nbsp;<a href="http://www.java1234.vip" target="_blank">www.java1234.vip</a>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+.footer{
+  padding: 20px;
+  display: flex;
+  align-items: center;
+}
+</style>
---
+++ src/views/layout/header/avatar.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-dropdown>
+    <span class="el-dropdown-link">
+      <el-avatar shape="square" :size="40" :src="squareUrl" />
+    </span>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item @click="logout">安全退出</el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useStore } from 'vuex'
+
+const squareUrl=ref("https://img95.699pic.com/xsj/0o/jb/th.jpg!/fw/700/watermark/url/L3hzai93YXRlcl9kZXRhaWwyLnBuZw/align/southeast")
+
+const store=useStore()
+
+const logout=()=>{
+  store.dispatch('logout')
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
